6ac57b4c7363a8fb8f7eef837471fa497cdc2e42
Hasnain Khan <mohasnainkhan148@gmail.com>
2025-06-30 14:38:30 +0530

    update

--- lib/controller/cart_controller.dart
+++ lib/controller/cart_controller.dart
@@ -18,6 +18,7 @@ class CartController extends GetxController {
 
   late dynamic productSnapshot;
   var products = [];
+  var vendors = [];
 
   var placingOrder = false.obs;
 
@@ -54,12 +55,14 @@ class CartController extends GetxController {
       'order_on_delivery': false,
       'total_amount': totalAmount,
       'orders': FieldValue.arrayUnion(products),
+      'vendors': FieldValue.arrayUnion(vendors),
     });
     placingOrder(false);
   }
 
   getProductDetail() {
     products.clear();
+    vendors.clear();
     for (var i = 0; i < productSnapshot.length; i++) {
       products.add({
         'color': productSnapshot[i]['color'],
@@ -69,6 +72,7 @@ class CartController extends GetxController {
         'qty': productSnapshot[i]['qty'],
         'title': productSnapshot[i]['title'],
       });
+      vendors.add(productSnapshot[i]['vendor_id']);
     }
   }
 
